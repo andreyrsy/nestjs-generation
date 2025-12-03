859e07df2789b1ec84fd30ae57924c602012dd91
andreyrsy <contatomainort@gmail.com>
2025-12-03 11:57:06 -0300

    feat: (crud na API) completo Create, Read, Update, Delete

--- src/postagem/services/postagem.service.ts
+++ src/postagem/services/postagem.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from "@nestjs/common";
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
 import { Postagem } from "../entities/postagem.entity";
+import { DeleteResult } from "typeorm/browser";
 
 @Injectable()
 export class PostagemSerivce {
@@ -12,5 +13,35 @@ export class PostagemSerivce {
 
     async findAll(): Promise<Postagem[]>{
         return await this.postagemRepository.find();
+    }
+
+    async findById(id: number): Promise<Postagem>{
+        const postagem = await this.postagemRepository.findOne({
+            where: {
+                id,
+            },
+        });
+
+        if(!postagem){
+            throw new HttpException("Postagem não encontrada!", HttpStatus.NOT_FOUND)
+        }
+        return postagem;
+    }
+
+    async create(postagem: Postagem): Promise<Postagem>{
+        return await this.postagemRepository.save(postagem);
+    }
+
+    async update(postagem: Postagem): Promise<Postagem>{
+
+        await this.findById(postagem.id);
+
+        return await this.postagemRepository.save(postagem);
+    }
+
+    async delete(id: number): Promise<DeleteResult>{
+        await this.findById(id);
+
+        return this.postagemRepository.delete(id);
     }
 }
